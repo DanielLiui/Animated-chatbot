f8ca8da0914ff801dcb123eb5f13e457ad12e202
Daniel Liu <danielliu545@gmail.com>
2024-05-19 09:39:59 -0400

    Initial commit

---
+++ public/evListeners.js
@@ -0,0 +1,72 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  let sendButton = document.querySelector(".send-button")
+  let inputField = document.querySelector(".input-field")
+  let messageArea = document.querySelector(".message-area")
+
+  function sendMessage() {
+    let message = inputField.value.trim()
+    console.log("Message: " + message)
+  
+    if (message !== '') {
+      inputField.value = ''
+      let messageDiv = document.createElement('div')
+      messageDiv.className = 'message'
+  
+      let img = document.createElement('img')
+      img.src = '../assets/porcupine.png'
+      messageDiv.appendChild(img)
+  
+      let messageSpan = document.createElement('span');
+      messageSpan.className = 'message-text'
+      messageSpan.textContent = message
+      messageDiv.appendChild(messageSpan)
+  
+      messageArea.appendChild(messageDiv)
+    }
+  }
+
+  /*
+  function getResponse() {
+    let message = inputField.value.trim()
+
+    if (message !== '') {
+      let reqData = {message: message} 
+      xr = new XMLHttpRequest()
+      xr.open("POST", "/getReponse")
+      xr.setRequestHeader("Content-Type", "application/json")
+      xr.send(JSON.stringify(reqData))
+
+      xr.onreadystatechange = () => {
+        if (xr.readyState == 4 && xr.status == 200) {
+          ret = JSON.parse(xr.responseText)
+          console.log("Resp from server: " + ret)
+
+          
+        }
+      }
+    }
+
+  }
+
+  */
+
+
+` `
+  sendButton.addEventListener("click", (ev)=> {
+    ev.preventDefault()
+    sendMessage()
+  });
+
+  inputField.addEventListener('keydown', (ev)=> {
+    if (ev.key === 'Enter') {
+      ev.preventDefault()
+      sendMessage()
+      getResponse()
+    }
+  });
+});
+
+
+  
