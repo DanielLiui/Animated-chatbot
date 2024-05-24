18d7f0f88808f0fbbdacb47c1d9abbd63de62f96
Daniel Liu <danielliu545@gmail.com>
2024-05-24 11:31:32 -0400

    Testing

---
+++ public/animationTest.js
@@ -0,0 +1,34 @@
+
+//+
+document.addEventListener("DOMContentLoaded", () => {
+  let botImg = document.querySelector("#bot-img")
+  let animateButton = document.querySelector("#animate-button")
+  let stopButton = document.querySelector("#stop-button")
+  let intervalId
+
+  function startAnimation() {
+    let imgs = ["K smile.png", "K grin.png", "K laugh 2.png"]
+    let img_i = 0
+    let fps = 1
+  
+    intervalID = setInterval(()=> {
+      botImg.src = "../assets/" + imgs[img_i];
+      img_i = (img_i + 1) % imgs.length
+    }, 1000/fps)
+  
+  }
+
+  function stopAnimation() {
+    clearInterval(intervalID);
+  }
+
+  //ev handlers
+  animateButton.addEventListener('click', (ev)=> {
+    startAnimation()
+  })
+
+  stopButton.addEventListener('click', (ev)=> {
+    stopAnimation()
+  })
+  
+})
